877dbb5e0153e4d36af0446e0aef8c6ba1b6ecd9
Jess Bradshaw <info@jessbradshaw.ca>
2025-03-06 16:39:46 -0500

    Adding in transfer to pattern scene

---
+++ Remember Me Knot/Assets/Scripts/SetPatternColours.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class SetPatternColours : MonoBehaviour
+{
+    public GameObject UIitem;
+    public GameObject UIitem2;
+    [SerializeField] public List<Color> listOfColours;
+
+    void Awake()
+    {
+        UIitem.GetComponent<Image>().color = listOfColours[PlayerData.playerEyes];
+        UIitem2.GetComponent<Image>().color = listOfColours[PlayerData.playerWoolColour];
+    }
+}
