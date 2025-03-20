d4ad372cd84ecfddb08a6fdf347a9e1210bbb8d5
keely-brown-sheridan <145293246+keely-brown-sheridan@users.noreply.github.com>
2025-03-20 13:17:13 -0400

    Fixed issue with the dialogue being immediately triggered when it's being finished

--- Remember Me Knot/Assets/Scripts/Dialogue Related/DialogueStarter.cs
+++ Remember Me Knot/Assets/Scripts/Dialogue Related/DialogueStarter.cs
@@ -4,10 +4,19 @@ using Yarn.Unity;
 public class DialogueStarter : MonoBehaviour
 {
     [SerializeField] private string conversationStartNode; 
-    public DialogueRunner dialogueRunner; 
+    public DialogueRunner dialogueRunner;
+
+
     //public GameObject inputPopup; 
-    public bool inside; 
-    
+    public bool inside;
+
+
+
+    [SerializeField]
+    private float readyDelay = 1f;
+    private bool readyToInteract = true;
+    private bool preparingToReady = false;
+
      private void OnTriggerEnter (Collider other)
     {
        if (other.CompareTag("Player"))
@@ -17,16 +26,30 @@ public class DialogueStarter : MonoBehaviour
     }
     private void Update()
     {
+        if (!readyToInteract && !dialogueRunner.IsDialogueRunning && !preparingToReady)
+        {
+            preparingToReady = true;
+            Invoke("SetReady", readyDelay);
+        }
+
         //check if the other collider is the player character 
-        if (inside && dialogueRunner != null && !dialogueRunner.IsDialogueRunning) 
+        if (inside && dialogueRunner != null && readyToInteract) 
         { 
             if(Input.GetKeyDown(KeyCode.Space))
             {
-            Debug.Log("F was pressed "+ gameObject.name + " with conversationStartNode " + conversationStartNode); 
-            dialogueRunner.StartDialogue(conversationStartNode); 
+                Debug.Log("F was pressed "+ gameObject.name + " with conversationStartNode " + conversationStartNode); 
+                dialogueRunner.StartDialogue(conversationStartNode);
+                readyToInteract = false;
             }
         }
     }
+
+    public void SetReady()
+    {
+        readyToInteract = true;
+        preparingToReady = false;
+    }
+    
      private void OnTriggerExit (Collider other)
     {
         //check if the other collider is the player character 
